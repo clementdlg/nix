019cc1697269bb093d2ad798d4cfcf5dde855ee7
Clément DE LA GENIERE <0xkrem@proton.me>
2026-01-24 02:42:13 +0100

    fix ansible-lsp wrapper

--- packages/ansible-lsp/flake.nix
+++ packages/ansible-lsp/flake.nix
@@ -6,27 +6,23 @@
 	};
 
 	outputs = { self, nixpkgs }:
-		let
+	let
 		system = "x86_64-linux";
-	pkgs = import nixpkgs { inherit system; };
+		pkgs = import nixpkgs { inherit system; };
 	in
 	{
-		packages.${system}.ansible-language-server =
-			pkgs.writeShellApplication {
-				name = "ansible-language-server";
+		packages.${system}.default = pkgs.writeShellApplication {
+			name = "ansible-language-server";
 
-				runtimeInputs = [
-					pkgs.nodejs
-						pkgs.vscode-extensions.redhat.ansible
-				];
+			runtimeInputs = [
+				pkgs.nodejs
+				pkgs.vscode-extensions.redhat.ansible
+			];
 
-				text = ''
-					exec ${pkgs.nodejs}/bin/node \
-					${pkgs.vscode-extensions.redhat.ansible}/share/vscode/extensions/redhat.ansible/out/server/src/server.js "$@"
-					'';
-			};
-
-		default =
-			self.packages.${system}.ansible-language-server;
+			text = ''
+				exec ${pkgs.nodejs}/bin/node \
+				${pkgs.vscode-extensions.redhat.ansible}/share/vscode/extensions/redhat.ansible/out/server/src/server.js "$@"
+			'';
+		};
 	};
 }
