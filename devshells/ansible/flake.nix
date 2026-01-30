56fbb8b6b8eb3234add12991c933211f9083fc8c
Clément DE LA GENIERE <0xkrem@proton.me>
2026-01-30 01:45:34 +0100

    added ansible-lsp to devshell

--- devshells/ansible/flake.nix
+++ devshells/ansible/flake.nix
@@ -11,12 +11,32 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+
+      packages.${system}.ansible-language-server = pkgs.writeShellApplication {
+        name = "ansible-language-server";
+
+        runtimeInputs = [
+          pkgs.bun
+            pkgs.vscode-extensions.redhat.ansible
+        ];
+
+        text = ''
+          exec ${pkgs.bun}/bin/bun \
+          ${pkgs.vscode-extensions.redhat.ansible}/share/vscode/extensions/redhat.ansible/out/server/src/server.js "$@"
+          '';
+      };
+
       devShells.${system}.default = pkgs.mkShell {
         packages = [
           pkgs.ansible_2_18
           pkgs.ansible-lint
           pkgs.ansible-language-server
+          self.packages.${system}.ansible-language-server
+		  pkgs.glibcLocales
         ];
+
+        LANG="C.UTF-8";
+        LC_ALL="C.UTF-8";
 
         shellHook = ''
           export PS1="(Ansible)$PS1"
